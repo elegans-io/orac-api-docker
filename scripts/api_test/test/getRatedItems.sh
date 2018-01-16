87606e2fc6528bbeac370d1fda7e063542290b28
Angelo Leto <angleto@gmail.com>
2018-01-16 16:50:08 +0100

    first commit

--- scripts/api_test/test/getRatedItems.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-ID=${1:-"99"}
-cat ../../movielens/ml-100k/u.data | grep -E "^${ID}\s" | cut -d$'\t' -f2 | while read line; do cat ../../movielens/ml-100k/u.item | grep -E "^$line\|"; done
-
--- scripts/api_test/test/getRecommendations.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-./recommendation/getUserRecommendations.sh 8888 index_0 99 | json_pp | grep item_id | grep -o -E "[0-9]+" | while read line ; do cat ../../movielens/ml-100k/u.item | grep -E "^$line\|"; done
-
